2de3fe4ce22bc8d922a31ffe8b9bbabbdd0945e9
Pedro Kamoda <pedeoksplays@gmail.com>
2020-09-03 16:25:56 -0300

    Estrutura Finalizada

---
+++ APIBoletim/APIBoletim/Repositories/AlunoRepository.cs
@@ -0,0 +1,120 @@
+﻿using APIBoletim.Context;
+using APIBoletim.Domains;
+using APIBoletim.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Repositories
+{
+    public class AlunoRepository : IAluno
+    {
+
+        BoletimContext conexao = new BoletimContext();
+
+        SqlCommand cmd = new SqlCommand();
+        public Aluno BuscarPorID(int id)
+        {
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText = "SELECT * FROM aluno WHERE IdAluno = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+
+            SqlDataReader dados = cmd.ExecuteReader();
+
+            Aluno aluno = new Aluno();
+
+            while(dados.Read())
+            {
+                aluno.IdAluno   = Convert.ToInt32(dados.GetValue(0));
+                aluno.Nome      = dados.GetValue(1).ToString();
+                aluno.RA        = dados.GetValue(2).ToString();
+                aluno.Idade     = Convert.ToInt32(dados.GetValue(3));
+            }
+            conexao.Desconectar();
+
+            return aluno;
+        }
+        
+        public Aluno Cadastrar(Aluno a)
+        {
+            cmd.Connection  = conexao.Conectar();
+
+            cmd.CommandText =
+                "INSERT INTO aluno (Nome, Ra, Idade)" +
+                "VALUES" +
+                "(@nome, @ra, @idade)";
+            cmd.Parameters.AddWithValue("@nome", a.Nome);
+            cmd.Parameters.AddWithValue("@ra", a.RA);
+            cmd.Parameters.AddWithValue("@idade", a.Idade);
+
+            cmd.ExecuteNonQuery();
+            conexao.Desconectar();
+
+            return a;
+        }
+
+        public Aluno Alterar(int id, Aluno a)
+        {
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText = "UPDATE aluno SET " +
+                "Nome  = @nome, " +
+                "Ra    = @ra, " +
+                "Idade = @idade WHERE IdAluno = @id";
+
+            cmd.Parameters.AddWithValue("id", id);
+
+            cmd.Parameters.AddWithValue("@nome", a.Nome);
+            cmd.Parameters.AddWithValue("@ra", a.RA);
+            cmd.Parameters.AddWithValue("@idade", a.Idade);
+
+            cmd.ExecuteNonQuery();
+
+            conexao.Desconectar();
+            return a;
+        }
+
+
+        public void Excluir(int id)
+        {
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText = "DELETE FROM Aluno WHERE IdAluno = @id";
+            cmd.Parameters.AddWithValue("@id", id);
+
+            cmd.ExecuteNonQuery();
+
+            conexao.Desconectar();
+        }
+        public List<Aluno> ListarTodos()
+        {
+            cmd.Connection = conexao.Conectar();
+
+            cmd.CommandText = "SELECT * FROM aluno";
+
+            SqlDataReader dados = cmd.ExecuteReader();
+
+            List<Aluno> alunos = new List<Aluno>();
+
+            while(dados.Read())
+            {
+                alunos.Add(
+                    new Aluno()
+                    {
+                        IdAluno = Convert.ToInt32(dados.GetValue(0)),
+                        Nome = dados.GetValue(1).ToString(),
+                        RA = dados.GetValue(2).ToString(),
+                        Idade = Convert.ToInt32(dados.GetValue(3))
+                    }
+
+                    );
+            }
+            conexao.Desconectar();
+            return alunos;
+        }
+    }
+}
+
---
+++ SQLBoletim/DML.sql
@@ -0,0 +1,29 @@
+  
+USE boletim;
+
+/* DML - Data Manipulation Language */
+--INSERT - Inserir dados
+INSERT INTO Aluno (Nome, Ra, Idade) VALUES ('Lucax', 'R1236', 16);
+INSERT INTO materia (Titulo) VALUES ('Biologia');
+
+-- Com FK's
+INSERT INTO trabalho 
+	(DataEntrega, IdAluno, IdMateria)
+	VALUES
+	('2020-08-06T23:59:59', 1, 1);
+
+-- UPDATE - Alterar dados
+UPDATE trabalho SET
+	Nota = 7.89
+WHERE IdTrabalho = 5;
+
+-- DELETE - Deletar dados
+DELETE FROM Materia WHERE IdMateria = 1;
+
+-- DQL de consulta simples
+SELECT * FROM Trabalho;
+SELECT * FROM Aluno;
+SELECT * FROM Materia;
+
+-- Alterar tabela para aceitar números quebrados:
+ALTER TABLE Trabalho ALTER COLUMN Nota DECIMAL (10, 2)
